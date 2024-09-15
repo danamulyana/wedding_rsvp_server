a776417dffc568aa0482536da630fa820f6be628
Dana Mulyana <danabontot@gmail.com>
2024-09-15 09:25:53 +0700

    #project# - config cors env

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ const cors = require('cors');
 // Load environment variables
 dotenv.config();
 
+const allowedOrigins = process.env.CORS_ORIGIN.split(',');
+
 const corsOptions = {
-    origin: ['http://bayudanputriwedding.vercel.app/', 'http://127.0.0.1:5500/'],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     optionsSuccessStatus: 200
-};
+  };
 
 app.use(cors(corsOptions));
 
